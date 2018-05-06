824c020a2ac3c493b78aa7c5fc91ca78b5c721c3
Michal <Michal.Jaworek@zooplus.com>
2018-05-06 21:56:16 +0200

    template changed

--- source/src/views/Home.vue
+++ source/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Keep calm and wait for this cool page"/>
   </div>
 </template>
 
