e25914490b821867c8f8074d5b29499b7d937a59
Michal <Michal.Jaworek@zooplus.com>
2018-05-06 21:52:14 +0200

    configuration sketch

--- source/src/store.js
+++ source/src/store.js
@@ -4,13 +4,52 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {
+        technologies: [
+            {
+                name: "s3",
+                parent: "aws"
+            },
+            {
+                name: "ec2",
+                parent: "aws"
+            },
+            {
+                name: "aws",
+                parent: "cloud"
+            },
+            {
+                name: "cloud"
+            },
+            {
+                name: "vuex",
+                parent: "vue"
+            },
+            {
+                name: "vue",
+                parent: "frontend"
+            },
+            {
+                name: "frontend"
+            }
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+        ],
+        projects: [
+            {
+                name: "technology matrix",
+                repo: "https://github.com/jaworekmichal/jaworekmichal.github.io",
+                prod: "http://jaworekmichal.github.io",
+                description: "This page",
+                maturity: "poc", // "idea", "poc", "privateUse", "publicUse",
+                technologies: ["vue", "vuex"]
+            }
+        ]
+    },
+    mutations: {},
+    actions: {},
+    getters: {
+        modelToDisplay(){
+            return {}
+        }
+    }
 })
